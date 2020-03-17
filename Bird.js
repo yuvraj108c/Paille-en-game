7caf4a820d189e5623a9a74f925aec04052dd250
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2020-03-17 10:19:45 +0400

    Bird flapping

---
+++ Bird.js
@@ -0,0 +1,14 @@
+class Bird {
+  constructor(url) {
+    this.url = url;
+    (this.sizeX = 150), (this.sizeY = 100);
+  }
+  getImage() {
+    let i = new Image();
+    i.src = this.url;
+    return i;
+  }
+  draw(ctx, x, y) {
+    ctx.drawImage(this.getImage(), x, y, this.sizeX, this.sizeY);
+  }
+}
--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
-c.height = window.innerHeight - 22;
+c.height = window.innerHeight - 100;
 c.width = window.innerWidth - 22;
+
+let time = 0;
+let flap_speed = 3;
+
+// Birds
+const bird_open = new Bird("./img/bird_open.png");
+const bird_close = new Bird("./img/bird_close.png");
+
+function draw() {
+  ctx.clearRect(0, 0, c.width, c.height);
+  time % flap_speed === 0
+    ? bird_open.draw(ctx, 10, 10)
+    : bird_close.draw(ctx, 10, 10);
+}
+
+function loop() {
+  time++;
+  draw();
+  outputParameters();
+}
+
+function outputParameters() {
+  document.getElementById("time").textContent = `Time: ${time}`;
+}
+
+setInterval(() => {
+  loop();
+}, 100);
